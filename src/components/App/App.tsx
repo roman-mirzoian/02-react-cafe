09aa12865a464a2662491a244328d6c1ef7c5d6f
Roman Mirzoian <r.mirzoian@nethunt.com>
2026-07-30 16:53:56 +0400

    refactor: reorganize App component structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import CafeInfo from './components/CafeInfo/CafeInfo'
-import VoteOptions from './components/VoteOptions/VoteOptions'
-import VoteStats from './components/VoteStats/VoteStats'
-import Notification from './components/Notification/Notification'
-import type { Votes, VoteType } from './types/votes'
+import CafeInfo from '../CafeInfo/CafeInfo'
+import VoteOptions from '../VoteOptions/VoteOptions'
+import VoteStats from '../VoteStats/VoteStats'
+import Notification from '../Notification/Notification'
+import type { Votes, VoteType } from '../../types/votes'
 import css from './App.module.css'
 
 function App() {
